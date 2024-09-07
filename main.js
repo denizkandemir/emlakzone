2f8b699a653834717e131702efaae052928128b2
Deniz Kandemir <denizkandemir137@gmail.com>
2024-09-07 23:21:15 +0300

    integrations styles updated

--- main.js
+++ main.js
@@ -3,39 +3,43 @@ const integrationsContainer = document.querySelector("#integrations-cards-contai
 const integrations = [
     {
         title: "Kontrat Takibi",
-        text: "",
+        text: "Emlak sektöründe kontrat takibi, kiralama ve satış süreçlerinin sorunsuz ilerlemesi için kritik öneme sahiptir. Doğru bir takip, anlaşmazlıkların önüne geçer, zamanında ödemeleri garanti altına alır ve süreçleri şeffaf hale getirir. Dijital yönetim, belge kaybını engeller.",
         img: "imgs/contract.jpg"
     },
 
     {
-        title: "SMTP Mail ve  Gmail Entegrasyonu ",
-        text: "",
-        img: "imgs/mailIntegration.jpg"
+        title: "Toplu Sms&Mail Gönderimi",
+        text: "Toplu SMS ve mail gönderimi, geniş kitlelere aynı anda hızlıca ulaşmanın en etkili yollarından biridir. Kampanyalar, duyurular veya hatırlatmalar için ideal bir çözüm sunar. Toplu mail ve sms göndererek, bilgilendirme ve pazarlama stratejilerinizi kolayca hayata geçirebilirsiniz.",
+        img: "imgs/sendingMail.jpg"
     },
+
+   
 
     {
         title: "Müşteri Takibi",
-        text: "",
+        text: "Müşteri takibi, satış sonrası müşteri memnuniyetini artırmak, sadakati güçlendirmek ve uzun vadeli ilişkiler kurmak için kritik bir süreçtir. Düzenli iletişim ve veri analizi, müşterilere uygun çözümler sunmaya ve müşteri memnuniyetini arttırmaya yardımcı olur.",
         img: "imgs/client.jpg"
     },
 
     {
-        title: "NETGSM Entragrasyonu ",
-        text: "",
+        title: "NETGSM Entragrasyonu",
+        text: "NETGSM entegrasyonu, işletmelerin SMS, sesli mesaj ve diğer iletişim hizmetlerini kendi sistemlerine entegre ederek hızlı ve etkili bir iletişim ağı kurmalarını sağlar. Otomatik mesaj gönderimi, toplu SMS kampanyaları ve müşteri bildirimleri gibi işlemler NETGSM ile kolayca yönetilir.",
         img: "imgs/netGsm.jpg"
     },
 
     {
         title: "Takvim",
-        text: "",
+        text: "Emlak takvimi, kiralama ve satış süreçlerinde önemli tarihleri takip etmeyi kolaylaştırır. Kira ödemeleri, kontrat yenilemeleri, bakım hizmetleri gibi kritik olayları hatırlatarak, hem mülk sahiplerinin hem de kiracıların zamanında aksiyon almasını sağlar.",
         img: "imgs/calender.jpg"
     },
 
     {
-        title: "Toplu Sms&Mail Gönderimi ",
-        text: "",
-        img: "imgs/sendingMail.jpg"
+        title: "SMTP Mail ve  Gmail Entegrasyonu",
+        text: "SMTP (Simple Mail Transfer Protocol) ve Gmail entegrasyonu, uygulamalar üzerinden güvenli e-posta gönderimini sağlar. Gmail'in SMTP sunucusu kullanılarak yapılan gönderimler, spam filtrelerinden geçer ve güvenli bir şekilde alıcılara ulaşır.",
+        img: "imgs/mailIntegration.jpg"
     },
+
+    
 ]
 
 integrationsContainer.innerHTML = integrations.map((integration) => {
@@ -48,11 +52,13 @@ integrationsContainer.innerHTML = integrations.map((integration) => {
         <p class="integrations-title"> ${integration.title} </p>
         <p class="integrations-p"> ${integration.text} </p>
       </div>
-      <div class="services-button-container">
-       <img class="services-logo" src="imgs/logo.png" alt="">
+      <div class="integrations-button-container">
+       <div class="integrations-logo-container">
+            <img class="integrations-logo" src="imgs/logo.png" alt="">
+       </div>
         <a
-           class="services-link" href="#/translatePage">
-           <button class="services-button"> İncele </button>
+           class="integrations-link">
+           <button class="integrations-button"> İncele </button>
        </a>
        </div>
    </div>  
