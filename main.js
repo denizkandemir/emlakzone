99526e4acaa1284d714157923efa69bb9b8b1908
Deniz Kandemir <denizkandemir137@gmail.com>
2024-09-14 19:59:50 +0300

    entegrations icons and styles added

--- main.js
+++ main.js
@@ -2,38 +2,38 @@ const integrations = [
     {
         title: "Kontrat Takibi",
         text: "Emlak sektöründe kontrat takibi, kiralama ve satış süreçlerinin sorunsuz ilerlemesi için kritik öneme sahiptir. Doğru bir takip, anlaşmazlıkların önüne geçer, zamanında ödemeleri garanti altına alır ve süreçleri şeffaf hale getirir. Dijital yönetim, belge kaybını engeller.",
-        img: "imgs/contract.jpg"
+        img: "imgs/contract-icon.png"
     },
 
    
     {
         title: "Müşteri Takibi",
         text: "Müşteri takibi, satış sonrası müşteri memnuniyetini artırmak, sadakati güçlendirmek ve uzun vadeli ilişkiler kurmak için kritik bir süreçtir. Düzenli iletişim ve veri analizi, müşterilere uygun çözümler sunmaya ve müşteri memnuniyetini arttırmaya yardımcı olur.",
-        img: "imgs/client.jpg"
+        img: "imgs/client-icon.png"
     },
 
     {
         title: "Toplu Sms&Mail Gönderimi",
         text: "Toplu SMS ve mail gönderimi, geniş kitlelere aynı anda hızlıca ulaşmanın en etkili yollarından biridir. Kampanyalar, duyurular veya hatırlatmalar için ideal bir çözüm sunar. Toplu mail ve sms göndererek, bilgilendirme ve pazarlama stratejilerinizi kolayca hayata geçirebilirsiniz.",
-        img: "imgs/sendingMail.jpg"
+        img: "imgs/mail-sending-icon.png"
     },
 
     {
         title: "NETGSM Entragrasyonu",
         text: "NETGSM entegrasyonu, işletmelerin SMS, sesli mesaj ve diğer iletişim hizmetlerini kendi sistemlerine entegre ederek hızlı ve etkili bir iletişim ağı kurmalarını sağlar. Otomatik mesaj gönderimi, toplu SMS kampanyaları ve müşteri bildirimleri gibi işlemler NETGSM ile kolayca yönetilir.",
-        img: "imgs/netGsm.jpg"
+        img: "imgs/netgsm-icon.png"
     },
 
     {
         title: "Takvim",
         text: "Emlak takvimi, kiralama ve satış süreçlerinde önemli tarihleri takip etmeyi kolaylaştırır. Kira ödemeleri, kontrat yenilemeleri, bakım hizmetleri gibi kritik olayları hatırlatarak, hem mülk sahiplerinin hem de kiracıların zamanında aksiyon almasını sağlar.",
-        img: "imgs/calender.jpg"
+        img: "imgs/calender-icon.png"
     },
 
     {
         title: "SMTP Mail ve  Gmail Entegrasyonu",
         text: "SMTP (Simple Mail Transfer Protocol) ve Gmail entegrasyonu, uygulamalar üzerinden güvenli e-posta gönderimini sağlar. Gmail'in SMTP sunucusu kullanılarak yapılan gönderimler, spam filtrelerinden geçer ve güvenli bir şekilde alıcılara ulaşır.",
-        img: "imgs/mailIntegration.jpg"
+        img: "imgs/stmpMail-icon.png"
     },
 
 
@@ -68,8 +68,10 @@ function handleIntegrations() {
       <div class="integrations-img-container">
         <img src="${integration.img}" class="integration-img" alt="">
       </div>
+      <div class="line-div-container">
+         <p class="integrations-title"> ${integration.title} </p>
+      </div>
       <div class="integrations-text-container">
-        <p class="integrations-title"> ${integration.title} </p>
         <p class="integrations-p"> ${integration.text} </p>
       </div>
       <div class="integrations-button-container">
@@ -147,6 +149,13 @@ function handleQuestionDropdowns() {
 
     questionDropdowns.forEach((dropdown) => {
         dropdown.addEventListener("click", () => {
+            questionDropdowns.forEach((prevDropdown) => {
+                if (prevDropdown !== dropdown) {
+                    const prevAnswer = prevDropdown.closest(".sss-question-answer-container").querySelector(".answer-p");
+                    prevAnswer.classList.remove("active");
+                    prevDropdown.classList.remove("icon-up");
+                }
+            })
             const answer = dropdown.closest(".sss-question-answer-container").querySelector(".answer-p");
             answer.classList.toggle("active");
             dropdown.classList.toggle("icon-up");
